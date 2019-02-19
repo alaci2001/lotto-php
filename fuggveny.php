c194e4434ba2c1a55de65b89beb500968c4b279a
diak <diak@10.9.8.9>
2019-02-19 08:00:17 +0100

    lottoszelvény fgv

--- fuggveny.php
+++ fuggveny.php
@@ -14,4 +14,33 @@
                 return $tomb;
                 //var_dump($tomb);   
              }
+             function lottoszelveny($sorsolt)
+{
+        $szamokszama=90;
+        $oszlop=15;
+        echo '<table border="1">';
+	for ($i=1; $i<=$szamokszama; $i++)
+	{
+		if ($i%$oszlop==1)
+		{
+			echo "<tr>\n";
+		}
+		if (isset($sorsolt[$i]))
+		{	
+			$hatterszin="#FF0000";
+		}
+		else
+		{	
+			$hatterszin="yellow";
+		}
+		echo "<td bgcolor=\"$hatterszin\">";
+			echo $i;
+		echo "</td>";	
+		if ($i%$oszlop==0)
+		{
+			echo "</tr>";
+		}
+	}
+	echo "</table>";
+}
 ?>
