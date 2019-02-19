a13387a2d54f05585408e7390b2bceae23edb1f2
diak <diak@10.9.8.9>
2019-02-19 07:59:51 +0100

    függvényhasználat, fgv áthelyezése fgv.php be

--- index.php
+++ index.php
@@ -11,33 +11,7 @@
             $sorsolt=sorsolas();
             var_dump($sorsolt);
 
-        $szamokszama=90;
-        $oszlop=15;
-        echo '<table border="1">';
-	for ($i=1; $i<=$szamokszama; $i++)
-	{
-		if ($i%$oszlop==1)
-		{
-			echo "<tr>\n";
-		}
-		if (isset($sorsolt[$i]))
-		{	
-			$hatterszin="#FF0000";
-		}
-		else
-		{	
-			$hatterszin="yellow";
-		}
-		echo "<td bgcolor=\"$hatterszin\">";
-			echo $i;
-		echo "</td>";	
-		if ($i%$oszlop==0)
-		{
-			echo "</tr>";
-		}
-	}
-	echo "</table>";
-           
+lottoszelveny($sorsolt);
         ?>
     </body>
 </html>
